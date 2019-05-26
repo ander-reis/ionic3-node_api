0f7a458da1d18bb5acb42cbb36550c74d3522ee2
Desktop Casa <ander-reis@hotmail.com>
2019-05-26 12:32:13 -0300

    Correções node-api

--- node-api/server.js
+++ node-api/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const app = require('../node-api/bin/express');
-const variable = require('../node-api/bin/configuration/variables');
+const app = require('./bin/express');
+const variable = require('./bin/configuration/variables');
 
 app.listen(variable.Api.port, () => {
     console.info(`Api inicializada com sucesso na porta ${variable.Api.port}`);
